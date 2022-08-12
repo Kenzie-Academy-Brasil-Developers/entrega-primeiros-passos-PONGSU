212264828007c205d3c9db4d8b399401170365ae
Gabriel <gabrielc.maciel42@gmail.com>
2022-08-12 17:01:00 -0300

    corigido 1

--- script.js
+++ script.js
@@ -1,53 +1,41 @@
 
-
-
+let mediaNota = 0
+let somaNota = 0
+continuar = true
 
 let nome = prompt("Digite aqui seu nome:")
 let tamanhoNome = nome.length
-if (tamanhoNome <= 4) {alert("ATENÇÂO O RESULTADO FOI PREJUDICADO! Atualize a página e se atente a seguinte informação: Precisamos que registre um nome com mais de 5 caracteres, tente adicionar o sobrenome!")}   
-//nao consegui ainda fazer a parar o script ou forçar que coloque o "valor" esperado no prompt
-
-
-let notaMatematica	 = parseFloat(prompt("Qual a nota em Matemática?"))
-let notaPortugues = parseFloat(prompt("Qual a nota em Português?"))
-let notaInformatica	 = parseFloat(prompt("Qual a nota em Informática?"))
-let notaGeografia	 = parseFloat(prompt("Qual a nota em Geografia?"))
-let notaIngles = parseFloat(prompt("Qual a nota em Ingles?"))
-let notaHistoria = parseFloat(prompt("Qual a nota em História?"))
-let notaFilosofia	 = parseFloat(prompt("Qual a nota em Filosofia?"))
-let notaSociologia = parseFloat(prompt("Qual a nota em Sociologia?"))
-let notaLiteratura	 = parseFloat(prompt("Qual a nota em Literatura?"))
-let notaEdFisica = parseFloat(prompt("Qual a nota em Educação Fisica?"))
-
-
-
-let presencaMatematica	 = parseFloat(prompt("Qual a nota de presença em Matemática?"))
-let presencaPortugues = parseFloat(prompt("Qual a nota de presença em Português?"))
-let presencaInformatica	 = parseFloat(prompt("Qual a nota de presença em Informática?"))
-let presencaGeografia	 = parseFloat(prompt("Qual a nota de presença em Geografia?"))
-let presencaIngles = parseFloat(prompt("Qual a nota de presença em Ingles?"))
-let presencaHistoria = parseFloat(prompt("Qual a nota de presença em História?"))
-let presencaFilosofia	 = parseFloat(prompt("Qual a nota de presença em Filosofia?"))
-let presencaSociologia = parseFloat(prompt("Qual a nota de presença em Sociologia?"))
-let presencaLiteratura	 = parseFloat(prompt("Qual a nota de presença em Literatura?"))
-let presencaEdFisica = parseFloat(prompt("Qual a nota de presença em Educação Fisica?"))
-
-let somaPresenca =  (presencaMatematica + presencaPortugues + presencaInformatica + presencaGeografia + presencaIngles + presencaHistoria + presencaFilosofia + presencaSociologia + presencaLiteratura + presencaEdFisica)
-let mediaPresenca = (somaPresenca / 10)
-
-let somaNota = (notaMatematica + notaPortugues + notaInformatica + notaGeografia + notaIngles + notaHistoria + notaFilosofia + notaSociologia + notaLiteratura + notaEdFisica)
-let mediaNota = (somaNota / 10)
-
-if (somaNota >= 1 || somaNota <= 100) {} 
-else {alert("ATENÇÂO O RESULTADO FOI PREJUDICADO! Atualize a página e se atente a seguinte informação: O valor das notas de matérias devem ser um numeral, no mínimo zero e no máximo 10")}
-if (somaPresenca >= 1 || somaPresenca <= 10) {} 
-else {alert("ATENÇÂO O RESULTADO FOI PREJUDICADO! Atualize a página e se atente a seguinte informação: O valor da nota de presença deve ser um numeral maior que zero e no máximo 10")}
-//em ambos os casos não acho que é a melhor forma de verificar se colocou o valor dentro do esperado entre 0 e 10
-//dessa forma ele avisa caso deixe algum em branco ou escreva, mas se colocar algum valor maior que 10 
-//vai passar "despercebido" se a soma total nao for maior que 100 :~
-
-
-if (mediaPresenca >= 6 && mediaNota >= 8) {alert(`A nota do aluno ${nome} é de ${mediaNota} e sua presença de ${mediaPresenca}: Aluno Aprovado!`)} 
-else {alert(`A nota do aluno ${nome} é de ${mediaNota} e sua presença de ${mediaPresenca}: Aluno Reprovado!`)}
-
-
+if (tamanhoNome <= 4) {alert("Precisamos que registre um nome com mais de 5 caracteres")} 
+else
+{presenca = prompt("Digite aqui o valor de sua presença:")}
+if (presenca <= 0 || presenca > 10) {alert("O valor da nota de presença deve ser um numeral maior que zero e no máximo 10")} 
+else {
+materia1 = prompt("Digite a Materia")
+nota1 = parseFloat(prompt(`Digite sua nota em ${materia1}`))
+if (nota1 >= 0 && nota1 <= 10) {materia2 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota2 = parseFloat(prompt(`Digite sua nota em ${materia2}`))
+if (nota2 >= 0 && nota2 <= 10) {materia3 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota3 = parseFloat(prompt(`Digite sua nota em ${materia3}`))
+if (nota3 >= 0 && nota3 <= 10) {materia4 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota4 = parseFloat(prompt(`Digite sua nota em ${materia4}`))
+if (nota4 >= 0 && nota4 <= 10) {materia5 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota5 = parseFloat(prompt(`Digite sua nota em ${materia5}`))
+if (nota5 >= 0 && nota5 <= 10) {materia6 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota6 = parseFloat(prompt(`Digite sua nota em ${materia6}`))
+if (nota6 >= 0 && nota6 <= 10) {materia7 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota7 = parseFloat(prompt(`Digite sua nota em ${materia7}`))
+if (nota7 >= 0 && nota7 <= 10) {materia8 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota8 = parseFloat(prompt(`Digite sua nota em ${materia8}`))
+if (nota8 >= 0 && nota8 <= 10) {materia9 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota9 = parseFloat(prompt(`Digite sua nota em ${materia9}`))
+if (nota9 >= 0 && nota9 <= 10) {materia10 = prompt("Digite a Materia")} else {alert("A nota deve ser um numeral entre 0 e 10")}
+nota10 = parseFloat(prompt(`Digite sua nota em ${materia10}`))
+if (nota10 >= 0 && nota10 <= 10)  
+    {
+    somaNota = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10)
+    mediaNota = (somaNota / 10) 
+if (presenca >= 6 && mediaNota >= 8) {alert(`A nota do aluno ${nome} é de ${mediaNota} e sua presença de ${presenca}: Aluno Aprovado!`)} 
+else {alert(`A nota do aluno ${nome} é de ${mediaNota} e sua presença de ${presenca}: Aluno Reprovado!`)}
+    }
+else {alert("A nota deve ser um numeral entre 0 e 10")}
+}
